83635b010ea26ba931d8b7a81721f02fc3a384c5
WalkerGeospatial <supahsceptile99@gmail.com>
2026-04-12 10:45:58 -0500

    Add favicon, suppress right-click context menu

--- src/App.jsx
+++ src/App.jsx
@@ -230,7 +230,7 @@ export default function App() {
   };
 
   return (
-    <div style={{ width: "100vw", height: "100vh", position: "relative" }}>
+    <div style={{ width: "100vw", height: "100vh", position: "relative" }} onContextMenu={e => e.preventDefault()}>
       <Header />
 
       <DeckGL
